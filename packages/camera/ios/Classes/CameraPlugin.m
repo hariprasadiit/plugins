ffd1f27b37f266abe34b629359a7e78fc52ff9e6
Hari Prasad <hariprasadiit@gmail.com>
2018-11-22 22:28:30 +0530

    - fixed orientation

--- packages/camera/ios/Classes/CameraPlugin.m
+++ packages/camera/ios/Classes/CameraPlugin.m
@@ -14,6 +14,88 @@
 }
 @end
 
+UIImage * fixOrientationOfImage(UIImage * image);
+
+UIImage * fixOrientationOfImage(UIImage * image) {
+    
+    // No-op if the orientation is already correct
+    if (image.imageOrientation == UIImageOrientationUp) return image;
+    
+    // We need to calculate the proper transformation to make the image upright.
+    // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (image.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, image.size.width, image.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, image.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, image.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
+    }
+    
+    switch (image.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, image.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, image.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, image.size.width, image.size.height,
+                                             CGImageGetBitsPerComponent(image.CGImage), 0,
+                                             CGImageGetColorSpace(image.CGImage),
+                                             CGImageGetBitmapInfo(image.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (image.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            // Grr...
+            CGContextDrawImage(ctx, CGRectMake(0,0,image.size.height,image.size.width), image.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,image.size.width,image.size.height), image.CGImage);
+            break;
+    }
+    
+    // And now we just create a new UIImage from the drawing context
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
+}
+
 @interface FLTSavePhotoDelegate : NSObject <AVCapturePhotoCaptureDelegate>
 @property(readonly, nonatomic) NSString *path;
 @property(readonly, nonatomic) FlutterResult result;
@@ -49,13 +131,16 @@
   NSData *data = [AVCapturePhotoOutput
       JPEGPhotoDataRepresentationForJPEGSampleBuffer:photoSampleBuffer
                             previewPhotoSampleBuffer:previewPhotoSampleBuffer];
-  // TODO(sigurdm): Consider writing file asynchronously.
-  bool success = [data writeToFile:_path atomically:YES];
-  if (!success) {
-    _result([FlutterError errorWithCode:@"IOError" message:@"Unable to write file" details:nil]);
-    return;
-  }
-  _result(nil);
+    UIImage *image = [UIImage imageWithData:data];
+    UIImage *rotatedImage = fixOrientationOfImage(image);
+    // TODO(sigurdm): Consider writing file asynchronously.
+    
+    bool success = [UIImagePNGRepresentation(rotatedImage) writeToFile:_path atomically:YES];
+    if (!success) {
+        _result([FlutterError errorWithCode:@"IOError" message:@"Unable to write file" details:nil]);
+        return;
+    }
+    _result(nil);
 }
 @end
 
@@ -174,6 +259,7 @@
         }
         
         AVCaptureConnection *connection = [captureStillImageOutput connectionWithMediaType:AVMediaTypeVideo];
+        
         [captureStillImageOutput captureStillImageAsynchronouslyFromConnection:connection completionHandler:^(CMSampleBufferRef  _Nullable sampleBuffer, NSError * _Nullable error) {
             
             if (error) {
@@ -181,8 +267,11 @@
                 return;
             }
             NSData *data =  [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:sampleBuffer];
+            UIImage *image = [UIImage imageWithData:data];
+            UIImage *rotatedImage = fixOrientationOfImage(image);
             // TODO(sigurdm): Consider writing file asynchronously.
-            bool success = [data writeToFile:path atomically:YES];
+            
+            bool success = [UIImagePNGRepresentation(rotatedImage) writeToFile:path atomically:YES];
             if (!success) {
                 result([FlutterError errorWithCode:@"IOError" message:@"Unable to write file" details:nil]);
                 return;
